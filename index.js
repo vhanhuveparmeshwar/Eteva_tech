6c1268bb1589de3b9cb6d3af9b907596b2602af8
vhanhuveparmeshwar <vhanhuveparmeshwar@gmail.com>
2023-07-26 15:36:54 +0000

    eteva tech company assignment

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
-let list1 = ['Arjun', 'Adwait', 'Swapnil', 'Amit', 'Vishal', 'Adnan'];
-let list2 = ['Adwait', 'Laxman', 'Amit', 'Adnan', 'Nitin', 'Gaurav'];
+const List1 = ["Arjun", "Adwait", "Swapnil", "Amit", "Vishal", "Adnan"];
+const List2 = ["Adwait", "Laxman", "Amit", "Adnan", "Nitin", "Gaurav"];
 
-let uniqueList1 = [], uniqueList2 = [], commonUsers = [];
-
-// a. Get a unique set of users from List1 which are not in List2
-for (let i = 0; i < list1.length; i++) {
-    let found = false;
-    for (let j = 0; j < list2.length; j++) {
-        if (list1[i] === list2[j]) {
-            found = true;
-            break;
-        }
-    }
-    if (!found) uniqueList1.push(list1[i]);
+const finalList1NotInList2 = [];
+for (let i of List1) {
+  if (!List2.includes(i)) {
+    finalList1NotInList2.push(i);
+  }
 }
+console.log(finalList1NotInList2);
 
-console.log(uniqueList1);
-
-// b. Get a unique set of users from List2 which are not in List1
-for (let i = 0; i < list2.length; i++) {
-    let found = false;
-    for (let j = 0; j < list1.length; j++) {
-        if (list2[i] === list1[j]) {
-            found = true;
-            break;
-        }
-    }
-    if (!found) uniqueList2.push(list2[i]);
+const finalList2NotInList1 = [];
+for (let i of List2) {
+  if (!List1.includes(i)) {
+    finalList2NotInList1.push(i);
+  }
 }
+console.log(finalList2NotInList1);
 
-console.log(uniqueList2);
-
-// c. Get a set of users who are present in both List1 and List2 (intersection of list1 & list2)
-for (let i = 0; i < list1.length; i++) {
-    for (let j = 0; j < list2.length; j++) {
-        if (list1[i] === list2[j]) {
-            commonUsers.push(list1[i]);
-            break;
-        }
-    }
+const finalList2IntersectionList1 = [];
+for (let i of List2) {
+  if (List1.includes(i)) {
+    finalList2IntersectionList1.push(i);
+  }
 }
-
-console.log(commonUsers);
+console.log(finalList2IntersectionList1);
